9cd3df05f1c2b803c67b3dec0a438bb96e0398f4
Joseph C. Caswell <josephcaswell@tpm.com>
2024-12-06 15:49:15 -0500

    Prettier Formatting

--- config.js
+++ config.js
@@ -1,72 +1,71 @@
 // Update these values to match those of your Server URL & DriveWorks Group Alias
 
 const config = {
-    // the full path the site running the DriveWorks Live API
-    serverUrl: "https://dw21api.tpmautomation.com",
-    // The default alias for the DriveWorks Group
-    // This is a custom string that must match the name in the ConfigUser.xml file
-    groupAlias: "PurAquaDEV",
-    // (Optional) Configure ping & update intervals - in seconds
-    // A Specification will timeout after a configured period of inactivity (see DriveWorksConfigUser.xml).
-    // This function prevents a Specification timing out as long as the page is in view.
-    // Disable the ping by setting to 0
-    specificationPingInterval: 0,
-    // (Optional) Enter custom redirect URLs for login/logout and Project/DriveApp close/cancel
-    folder: "PurAqua",
-    login: {
-        redirectUrl: "projects.html",
-    },
-    logout: {
-        redirectUrl: "index.html",
-    },
-    project: {
-        redirectOnClose: "details.html",
-        redirectOnCancel: "projects.html",
-    },
-    // (Optional) Configure 'Run' view
-    run: {
-        showWarningOnExit: true, // Toggle warning dialog when exiting "Run" view with potentially unsaved changes (where supported)
-        loadCustomProjectAssets: {
-            scripts: false,
-            styles: false,
-        },
-    },
-    // (Optional) Configure 'Details' view
-    details: {
-        updateInterval: 5, // Interval to refresh content - in seconds
-        showStartNewSpecificationAction: true,
-    },
-    // (Optional) Configure the query function
-    // Enter a default Group Alias and/or Project name to be used (when none are passed in the query string)
-    // Choose how sessions are handled
-    query: {
-        defaultGroupAlias: "PurAquaDEV",
-        defaultProjectName: "",
-        autoLogin: false,
-        requireNewSession: true,
-        requireExactAlias: false,
-    },
-    loginReturnUrls: true, // Toggle appending return urls to restore the previous location when redirected to the login form 
-    locale: "en-US", // Set the default locale for displaying dates and numbers
-    dateFormat: {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-    },
-    // Whether to show debugging information in the console
-    debug: false,
-    allowSingleSignOn: false,
-    accountManagement: {
-        allowForgotPassword: false,
-        allowChangePassword: false,
-        allowCreateAccount: false,
-    },
-    logo: {
-        login: "dist/img/PuraquaLogo_1_1.png",
-        sidebar: "dist/img/PuraquaLogo_1_1.png",
-    },
-
-};
+	// the full path the site running the DriveWorks Live API
+	serverUrl: "https://dw21api.tpmautomation.com",
+	// The default alias for the DriveWorks Group
+	// This is a custom string that must match the name in the ConfigUser.xml file
+	groupAlias: "PurAquaDEV",
+	// (Optional) Configure ping & update intervals - in seconds
+	// A Specification will timeout after a configured period of inactivity (see DriveWorksConfigUser.xml).
+	// This function prevents a Specification timing out as long as the page is in view.
+	// Disable the ping by setting to 0
+	specificationPingInterval: 0,
+	// (Optional) Enter custom redirect URLs for login/logout and Project/DriveApp close/cancel
+	folder: "PurAqua",
+	login: {
+		redirectUrl: "projects.html",
+	},
+	logout: {
+		redirectUrl: "index.html",
+	},
+	project: {
+		redirectOnClose: "details.html",
+		redirectOnCancel: "projects.html",
+	},
+	// (Optional) Configure 'Run' view
+	run: {
+		showWarningOnExit: true, // Toggle warning dialog when exiting "Run" view with potentially unsaved changes (where supported)
+		loadCustomProjectAssets: {
+			scripts: false,
+			styles: false,
+		},
+	},
+	// (Optional) Configure 'Details' view
+	details: {
+		updateInterval: 5, // Interval to refresh content - in seconds
+		showStartNewSpecificationAction: true,
+	},
+	// (Optional) Configure the query function
+	// Enter a default Group Alias and/or Project name to be used (when none are passed in the query string)
+	// Choose how sessions are handled
+	query: {
+		defaultGroupAlias: "PurAquaDEV",
+		defaultProjectName: "",
+		autoLogin: false,
+		requireNewSession: true,
+		requireExactAlias: false,
+	},
+	loginReturnUrls: true, // Toggle appending return urls to restore the previous location when redirected to the login form
+	locale: "en-US", // Set the default locale for displaying dates and numbers
+	dateFormat: {
+		month: "long",
+		day: "numeric",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	},
+	// Whether to show debugging information in the console
+	debug: false,
+	allowSingleSignOn: false,
+	accountManagement: {
+		allowForgotPassword: false,
+		allowChangePassword: false,
+		allowCreateAccount: false,
+	},
+	logo: {
+		login: "dist/img/PuraquaLogo_1_1.png",
+		sidebar: "dist/img/PuraquaLogo_1_1.png",
+	},
+}
--- dist/js/login.js
+++ dist/js/login.js
@@ -2,97 +2,97 @@
  * LOGIN
  */
 
-const SERVER_URL = config.serverUrl;
-const LOGIN_REDIRECT_URL = config.login.redirectUrl;
-const GROUP_ALIAS = config.groupAlias;
-const URL_QUERY = new URLSearchParams(window.location.search);
+const SERVER_URL = config.serverUrl
+const LOGIN_REDIRECT_URL = config.login.redirectUrl
+const GROUP_ALIAS = config.groupAlias
+const URL_QUERY = new URLSearchParams(window.location.search)
 
 // Elements
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-button");
-const loginSSOButton = document.getElementById("login-sso-button");
-const loginDivider = document.getElementById("login-divider");
-const loginNotice = document.getElementById("login-notice");
-const forgotLink = document.getElementById("forgot-link");
-const createLink = document.getElementById("create-link");
+const loginForm = document.getElementById("login-form")
+const loginButton = document.getElementById("login-button")
+const loginSSOButton = document.getElementById("login-sso-button")
+const loginDivider = document.getElementById("login-divider")
+const loginNotice = document.getElementById("login-notice")
+const forgotLink = document.getElementById("forgot-link")
+const createLink = document.getElementById("create-link")
 
 // Error Messages
-const genericErrorMessage = "There has been an issue.";
-const clientErrorMessage = "Cannot access client.";
-const privateErrorMessage = "Please use a non-private window.";
+const genericErrorMessage = "There has been an issue."
+const clientErrorMessage = "Cannot access client."
+const privateErrorMessage = "Please use a non-private window."
 
 // DriveWorks Live Client
-let client;
+let client
 
 /**
  * On page load.
  */
-(async function () {
-    setLogo();
+;(async function () {
+	setLogo()
 
-    loginForm.addEventListener("submit", handleLoginForm);
+	loginForm.addEventListener("submit", handleLoginForm)
 
-    if (loginSSOButton) {
-        if (config.allowSingleSignOn) {
-            loginSSOButton.addEventListener("click", handleLoginSSO);
-        } else {
-            loginSSOButton.style.display = "none";
-            loginDivider.style.display = "none";
-        }
-    }
+	if (loginSSOButton) {
+		if (config.allowSingleSignOn) {
+			loginSSOButton.addEventListener("click", handleLoginSSO)
+		} else {
+			loginSSOButton.style.display = "none"
+			loginDivider.style.display = "none"
+		}
+	}
 
-    if (forgotLink) {
-        if (!config.accountManagement.allowForgotPassword) {
-            forgotLink.style.display = "none";
-        }
-    }
+	if (forgotLink) {
+		if (!config.accountManagement.allowForgotPassword) {
+			forgotLink.style.display = "none"
+		}
+	}
 
-    if (createLink) {
-        if (!config.accountManagement.allowCreateAccount) {
-            createLink.style.display = "none";
-        }
-    }
-    showLoginNotice();
-})();
+	if (createLink) {
+		if (!config.accountManagement.allowCreateAccount) {
+			createLink.style.display = "none"
+		}
+	}
+	showLoginNotice()
+})()
 
 /**
  * Create client.
  */
 async function dwClientLoaded() {
-    try {
-        client = new window.DriveWorksLiveClient(SERVER_URL);
-    } catch (error) {
-        loginError(clientErrorMessage, error);
-    }
+	try {
+		client = new window.DriveWorksLiveClient(SERVER_URL)
+	} catch (error) {
+		loginError(clientErrorMessage, error)
+	}
 
-    // Quick Logout (?bye)
-    // https://docs.driveworkspro.com/Topic/WebThemeLogout
-    if (URL_QUERY.has("bye")) {
-        await forceLogout();
-    }
+	// Quick Logout (?bye)
+	// https://docs.driveworkspro.com/Topic/WebThemeLogout
+	if (URL_QUERY.has("bye")) {
+		await forceLogout()
+	}
 
-    startPageFunctions();
+	startPageFunctions()
 }
 
 /**
  * Start page functions.
  */
 function startPageFunctions() {
-    handlePasswordToggle();
+	handlePasswordToggle()
 
-    // Check localStorage support (show warning if not e.g. <= iOS 10 Private Window)
-    if (!localStorageSupported()) {
-        document.getElementById("login-button").disabled = true;
-        loginError(privateErrorMessage);
-        return;
-    }
+	// Check localStorage support (show warning if not e.g. <= iOS 10 Private Window)
+	if (!localStorageSupported()) {
+		document.getElementById("login-button").disabled = true
+		loginError(privateErrorMessage)
+		return
+	}
 
-    try {
-        // Check if logged in, and redirect
-        checkExistingLogin();
-    } catch (error) {
-        handleGenericError(error);
-    }
+	try {
+		// Check if logged in, and redirect
+		checkExistingLogin()
+	} catch (error) {
+		handleGenericError(error)
+	}
 }
 
 /**
@@ -101,96 +101,95 @@ function startPageFunctions() {
  * @param {Object} event - Form submit event.
  */
 async function handleLoginForm(event) {
+	// Prevent default form handling
+	event.preventDefault()
 
-    // Prevent default form handling
-    event.preventDefault();
+	// Show error if cannot connect to client
+	if (!client) {
+		loginError(clientErrorMessage)
+		return
+	}
 
-    // Show error if cannot connect to client
-    if (!client) {
-        loginError(clientErrorMessage);
-        return;
-    }
+	// Get credentials
+	const inputUsername = document.getElementById("login-username").value
+	const inputPassword = document.getElementById("login-password").value
+	const userCredentials = {
+		username: inputUsername,
+		password: inputPassword,
+	}
 
-    // Get credentials
-    const inputUsername = document.getElementById("login-username").value;
-    const inputPassword = document.getElementById("login-password").value;
-    const userCredentials = {
-        username: inputUsername,
-        password: inputPassword
-    };
+	try {
+		// Show loading state, reset notice
+		loginButton.classList.add("is-loading")
+		hideLoginNotice()
 
-    try {
-        // Show loading state, reset notice
-        loginButton.classList.add("is-loading");
-        hideLoginNotice();
+		// Start Session
+		const result = await client.loginGroup(GROUP_ALIAS, userCredentials)
 
-        // Start Session
-        const result = await client.loginGroup(GROUP_ALIAS, userCredentials);
+		// Show error is login failed
+		if (!result) {
+			loginError(genericErrorMessage)
+			return
+		}
 
-        // Show error is login failed
-        if (!result) {
-            loginError(genericErrorMessage);
-            return;
-        }
-
-        loginSuccess(result, inputUsername);
-    } catch (error) {
-        loginError(genericErrorMessage, error);
-    }
+		loginSuccess(result, inputUsername)
+	} catch (error) {
+		loginError(genericErrorMessage, error)
+	}
 }
 
 /**
  * Handle DriveWorks Group login via Single Sign-On (SSO).
  */
 async function handleLoginSSO() {
+	// Show error if cannot connect to client
+	if (!client) {
+		loginError(clientErrorMessage)
+		return
+	}
 
-    // Show error if cannot connect to client
-    if (!client) {
-        loginError(clientErrorMessage);
-        return;
-    }
+	try {
+		hideLoginNotice()
 
-    try {
-        hideLoginNotice();
+		// Start session
+		const result = await client.loginSSO(GROUP_ALIAS)
 
-        // Start session
-        const result = await client.loginSSO(GROUP_ALIAS);
+		// Show error is login failed
+		if (!result) {
+			loginError(genericErrorMessage)
+			return
+		}
 
-        // Show error is login failed
-        if (!result) {
-            loginError(genericErrorMessage);
-            return;
-        }
-
-        loginSuccess(result);
-    } catch (error) {
-        loginError(genericErrorMessage, error);
-    }
+		loginSuccess(result)
+	} catch (error) {
+		loginError(genericErrorMessage, error)
+	}
 }
 
 /**
  * Handle successful login. Store Session data to localStorage & redirect.
  */
 function loginSuccess(result, username) {
+	// Store session details to localStorage
+	localStorage.setItem("sessionId", result.sessionId)
+	localStorage.setItem("sessionAlias", GROUP_ALIAS)
 
-    // Store session details to localStorage
-    localStorage.setItem("sessionId", result.sessionId);
-    localStorage.setItem("sessionAlias", GROUP_ALIAS);
+	if (username) {
+		localStorage.setItem("sessionUsername", username)
+	}
 
-    if (username) {
-        localStorage.setItem("sessionUsername", username);
-    }
+	// Return to previous location (if redirected to login)
+	const returnUrl = URL_QUERY.get("returnUrl")
 
-    // Return to previous location (if redirected to login)
-    const returnUrl = URL_QUERY.get("returnUrl");
+	if (returnUrl && config.loginReturnUrls) {
+		window.location.href = `${window.location.origin}/${decodeURIComponent(
+			returnUrl,
+		)}`
+		return
+	}
 
-    if (returnUrl && config.loginReturnUrls) {
-        window.location.href = `${window.location.origin}/${decodeURIComponent(returnUrl)}`;
-        return;
-    }
-
-    // Redirect to default location
-    window.location.href = LOGIN_REDIRECT_URL;
+	// Redirect to default location
+	window.location.href = LOGIN_REDIRECT_URL
 }
 
 /**
@@ -200,16 +199,16 @@ function loginSuccess(result, username) {
  * @param {Object} [error] - The error object.
  */
 function loginError(noticeText, error = null) {
-    if (error) {
-        handleGenericError(error);
-    }
+	if (error) {
+		handleGenericError(error)
+	}
 
-    // Remove loading state
-    loginButton.classList.remove("is-loading");
+	// Remove loading state
+	loginButton.classList.remove("is-loading")
 
-    // Show client error
-    setLoginNotice(noticeText, "error");
-    showLoginNotice();
+	// Show client error
+	setLoginNotice(noticeText, "error")
+	showLoginNotice()
 }
 
 /**
@@ -219,102 +218,103 @@ function loginError(noticeText, error = null) {
  * @param {string} [state] - The type of message state (error/success/info).
  */
 function setLoginNotice(text, state = "info") {
-    const notice = JSON.stringify({text: text, state: state});
-    localStorage.setItem("loginNotice", notice);
+	const notice = JSON.stringify({text: text, state: state})
+	localStorage.setItem("loginNotice", notice)
 }
 
 /**
  * Show notice on login form.
  */
 function showLoginNotice() {
-    const notice = JSON.parse(localStorage.getItem("loginNotice"));
+	const notice = JSON.parse(localStorage.getItem("loginNotice"))
 
-    if (!notice) {
-        return;
-    }
+	if (!notice) {
+		return
+	}
 
-    let state = notice.state;
+	let state = notice.state
 
-    if (!state) {
-        state = "neutral";
-    }
+	if (!state) {
+		state = "neutral"
+	}
 
-    // Display feedback
-    loginNotice.innerText = notice.text;
-    loginNotice.classList.remove("error", "success", "neutral");
-    loginNotice.classList.add(state, "is-shown");
+	// Display feedback
+	loginNotice.innerText = notice.text
+	loginNotice.classList.remove("error", "success", "neutral")
+	loginNotice.classList.add(state, "is-shown")
 
-    // Clear message
-    localStorage.removeItem("loginNotice");
+	// Clear message
+	localStorage.removeItem("loginNotice")
 }
 
 /**
  * Hide notice on login form.
  */
 function hideLoginNotice() {
-    loginNotice.classList.remove("is-shown");
+	loginNotice.classList.remove("is-shown")
 }
 
 /**
  * Handle password visibility toggle.
  */
 function handlePasswordToggle() {
-    const passwordToggle = document.getElementById("password-toggle");
+	const passwordToggle = document.getElementById("password-toggle")
 
-    passwordToggle.onclick = function () {
-        const passwordInput = document.getElementById("login-password");
-        const currentType = passwordInput.type;
+	passwordToggle.onclick = function () {
+		const passwordInput = document.getElementById("login-password")
+		const currentType = passwordInput.type
 
-        if (currentType === "password") {
-            passwordInput.type = "text";
-            passwordToggle.innerHTML = '<svg class="icon"><use xlink:href="dist/icons.svg#eye-closed"/></svg> Hide';
-            return;
-        }
+		if (currentType === "password") {
+			passwordInput.type = "text"
+			passwordToggle.innerHTML =
+				'<svg class="icon"><use xlink:href="dist/icons.svg#eye-closed"/></svg> Hide'
+			return
+		}
 
-        passwordInput.type = "password";
-        passwordToggle.innerHTML = '<svg class="icon"><use xlink:href="dist/icons.svg#eye-open"/></svg> Show';
-    };
+		passwordInput.type = "password"
+		passwordToggle.innerHTML =
+			'<svg class="icon"><use xlink:href="dist/icons.svg#eye-open"/></svg> Show'
+	}
 }
 
 /**
  * Check existing login. Automatically login if found.
  */
 async function checkExistingLogin() {
-    const storedGroupAlias = localStorage.getItem("sessionAlias");
+	const storedGroupAlias = localStorage.getItem("sessionAlias")
 
-    if (!storedGroupAlias) {
-        return;
-    }
+	if (!storedGroupAlias) {
+		return
+	}
 
-    try {
-        // Test connection
-        await client.getProjects(storedGroupAlias, "$top=1");
+	try {
+		// Test connection
+		await client.getProjects(storedGroupAlias, "$top=1")
 
-        // Redirect to initial location
-        window.location.replace(LOGIN_REDIRECT_URL);
-    } catch (error) {
-        handleGenericError(error);
-    }
+		// Redirect to initial location
+		window.location.replace(LOGIN_REDIRECT_URL)
+	} catch (error) {
+		handleGenericError(error)
+	}
 }
 
 /**
  * Force logout and session data clearing.
  */
 async function forceLogout() {
+	// Logout from all Groups.
+	try {
+		await client.logoutAllGroups()
+	} catch (error) {
+		handleGenericError(error)
+	}
 
-    // Logout from all Groups.
-    try {
-        await client.logoutAllGroups();
-    } catch (error) {
-        handleGenericError(error);
-    }
+	// Clear session information from storage.
+	localStorage.clear()
 
-    // Clear session information from storage.
-    localStorage.clear();
-
-    // Show login screen message.
-    setLoginNotice("You have been logged out.", "success");
-    showLoginNotice();
+	// Show login screen message.
+	setLoginNotice("You have been logged out.", "success")
+	showLoginNotice()
 }
 
 /**
@@ -322,13 +322,13 @@ async function forceLogout() {
  * Example: Incognito (Private) windows in iOS 10 and below do not allow localStorage, errors when accessed.
  */
 function localStorageSupported() {
-    try {
-        localStorage.setItem("storageSupportTest", "Test");
-        localStorage.removeItem("storageSupportTest");
-        return true;
-    } catch (e) {
-        return false;
-    }
+	try {
+		localStorage.setItem("storageSupportTest", "Test")
+		localStorage.removeItem("storageSupportTest")
+		return true
+	} catch (e) {
+		return false
+	}
 }
 
 /**
@@ -337,16 +337,16 @@ function localStorageSupported() {
  * @param {Object} error - The error object.
  */
 function handleGenericError(error) {
-    console.log(error);
+	console.log(error)
 }
 
 /**
  * Set the logo
  */
 function setLogo() {
-    const logo = document.getElementById("logo");
+	const logo = document.getElementById("logo")
 
-    if (logo && config.logo.login) {
-        logo.src = config.logo.login;
-    }
+	if (logo && config.logo.login) {
+		logo.src = config.logo.login
+	}
 }
